e9730db0e123bb3004c85cce899f2ccb612f4270
Travis Crampton <crampton.travis@gmail.com>
2018-12-24 15:26:20 -0800

    handle having the correct url

--- src/agent.js
+++ src/agent.js
@@ -6,7 +6,8 @@ import DropDownHolder from "utils/DropdownHolder"
 import { logOut } from "auth"
 
 const ql = require("superagent-graphql")
-export const API_ROOT = "http://192.168.7.23:3000"
+// export const API_ROOT = "http://192.168.7.23:3000"
+export const API_ROOT = "https://aqueous-sea-94280.herokuapp.com"
 
 const responseBody = res => res.body.data
 
