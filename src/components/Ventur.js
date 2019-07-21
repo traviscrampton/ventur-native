e31fc4c6355ec80d1745139b0231e45b8bf8e719
Travis Crampton <crampton.travis@gmail.com>
2019-07-21 20:10:08 +0200

    try makeUrl

--- src/components/Ventur.js
+++ src/components/Ventur.js
@@ -80,8 +80,7 @@ class Ventur extends Component {
   async setCurrentUser() {
     try {
       let user = await AsyncStorage.getItem("currentUser")
-      const linkingUrl = await Linking.getInitialURL("/")
-
+      const linkingUrl = await Expo.Linking.makeUrl("venturAuth", {})
       user = JSON.parse(user)
       user = Object.assign({}, user, { linkingUrl })
       this.props.setCurrentUser(user)
