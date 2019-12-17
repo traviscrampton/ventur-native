24b7838c384fdb3b542400e54320131ec2543aaf
Travis Crampton <crampton.travis@gmail.com>
2019-12-17 12:18:09 -0800

    add a second photo default

--- src/assets/images/stockPhotos.js
+++ src/assets/images/stockPhotos.js
@@ -1,2 +1,6 @@
-export const journalBackground =
-  'https://ventur-serverless.s3.amazonaws.com/topo.jpg';
+const journalBackground = 'https://ventur-serverless.s3.amazonaws.com/topo.jpg';
+
+const gearDefault =
+  'https://ventur-serverless.s3.amazonaws.com/gearstockphoto.png';
+
+export { journalBackground, gearDefaultPhoto };
